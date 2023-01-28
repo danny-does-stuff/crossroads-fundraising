5d19673ff354e0b9e1f81bd2f344568a495f4d06
Danny Harding <dannyharding10@gmail.com>
2023-01-28 15:34:19 -0600

    Fix cypress tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -36,10 +36,14 @@ describe("smoke tests", () => {
     cy.findByRole("link", { name: /order mulch/i }).click();
     cy.findByText("No orders yet");
 
-    cy.findByRole("link", { name: /\+ new note/i }).click();
+    cy.findByRole("link", { name: /\+ new order/i }).click();
 
-    cy.findByRole("textbox", { name: /quantity/i }).type("4");
-    cy.findByRole("button", { name: /save/i }).click();
+    cy.findByLabelText(/number of bags/i).type("4");
+    // cy.findByRole("textbox", { name: /number of bags/i }).type("4");
+    cy.findByRole("textbox", { name: /street/i }).type(faker.address.streetName());
+    cy.findByRole("textbox", { name: /city/i }).type(faker.address.city());
+    cy.findByRole("textbox", { name: /zip/i }).type(faker.address.zipCode());
+    cy.findByRole("button", { name: /go to payment/i }).click();
 
     cy.findByRole("button", { name: /delete/i }).click();
 
