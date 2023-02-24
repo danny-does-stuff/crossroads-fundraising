49334f2ddc740b1c0d2c3465fe03310c71e668d8
Danny Harding <dannyharding10@gmail.com>
2023-02-23 21:46:17 -0600

    Style cards on admin page

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -19,6 +19,11 @@ function getOrderGrossIncome(order: CompleteOrder) {
   return order.pricePerUnit * order.quantity;
 }
 
+const currencyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
 function getTotalGrossIncome(
   orders: CompleteOrder[],
   onlyPaid: boolean = true
@@ -34,20 +39,35 @@ function getTotalGrossIncome(
 export default function Admin() {
   const { orders } = useTypedLoaderData<typeof loader>();
 
+  const paidOrders = orders.filter((o) => o.status === "PAID");
+
   return (
     <div className="p-6">
-      <div className="flex">
+      <div className="flex flex-col gap-4 sm:flex-row sm:[&>*]:flex-1">
         <RoundedBorder>
-          <h2>${getTotalGrossIncome(orders)}</h2>
-          Gross Income
+          <span className="text-5xl sm:text-6xl">
+            {currencyFormatter.format(getTotalGrossIncome(orders))}
+          </span>
+          <h2 className="text-2xl sm:text-4xl">Gross Income</h2>
         </RoundedBorder>
-        <RoundedBorder>
-          Total Orders: {orders.length}
+        <RoundedBorder className="text-2xl sm:text-4xl [&>hr]:my-2 [&>span]:float-right">
+          Total Orders: <span>{orders.length}</span>
+          <hr />
+          Total Paid Orders: <span>{paidOrders.length}</span>
           <hr />
           Total Bags:{" "}
-          {orders.reduce((total, order) => total + order.quantity, 0)}
+          <span>
+            {orders.reduce((total, order) => total + order.quantity, 0)}
+          </span>
           <hr />
-          Avg. Order: ${getTotalGrossIncome(orders, false) / orders.length}
+          Avg. Paid Order:{" "}
+          <span>
+            {currencyFormatter.format(
+              paidOrders.length
+                ? getTotalGrossIncome(orders) / paidOrders.length
+                : 0
+            )}
+          </span>
         </RoundedBorder>
       </div>
       <h2>All Orders</h2>
@@ -55,6 +75,16 @@ export default function Admin() {
   );
 }
 
-function RoundedBorder({ children }: { children: React.ReactNode }) {
-  return <div className="rounded border border-gray-300 p-4">{children}</div>;
+function RoundedBorder({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={`rounded-md border-gray-200 p-4 shadow-lg ${className}`}>
+      {children}
+    </div>
+  );
 }
