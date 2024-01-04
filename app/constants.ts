0e65376a91fdb0c3da6e983ecc71a1a3ea7dfb7d
Danny Harding <dannyharding10@gmail.com>
2024-01-03 18:19:18 -0600

    Set accepting mulch orders to true

--- app/constants.ts
+++ app/constants.ts
@@ -1,7 +1,7 @@
 export const CONTACT_EMAIL = "cr.youth.fundraising@gmail.com";
 
 // When false, the mulch order form will not be available.
-export const ACCEPTING_MULCH_ORDERS = false;
+export const ACCEPTING_MULCH_ORDERS = true;
 
 // The next year that mulch orders will be accepted. Only used if ACCEPTING_MULCH_ORDERS is true.
-export const NEXT_MULCH_YEAR = 2024;
+export const NEXT_MULCH_YEAR = 2025;
