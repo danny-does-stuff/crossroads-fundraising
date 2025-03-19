210927ca56853b3d428ac7f32d806f98a3a62091
Danny Harding <dannyharding10@gmail.com>
2025-03-19 10:07:24 -0500

    fix: disable mulch ordering

--- app/constants.ts
+++ app/constants.ts
@@ -1,7 +1,8 @@
 export const CONTACT_EMAIL = "cr.youth.fundraising@gmail.com";
 
-// When false, the mulch order form will not be available.
-export const ACCEPTING_MULCH_ORDERS = true;
+// Toggle this to allow/disallow ordering mulch.
+// When false, the mulch order form will not be available. And will be replaced with a thank you message.
+export const ACCEPTING_MULCH_ORDERS = false;
 
 // The next year that mulch orders will be accepted. Only used if ACCEPTING_MULCH_ORDERS is true.
 export const NEXT_MULCH_YEAR = 2026;
