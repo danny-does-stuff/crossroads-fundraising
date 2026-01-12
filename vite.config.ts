39f350742b5fc71c6c8dae72441eabe4b4ce6f5e
Danny Harding <dannyharding10@gmail.com>
2026-01-11 22:04:16 -0600

    ci: introduce nitro for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -14,6 +15,7 @@ export default defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tanstackStart(),
+    nitro(),
     // react's vite plugin must come after start's vite plugin
     viteReact({
       babel: {
